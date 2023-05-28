7b0abfe41202b80a4f11079b8008ec7a84182643
MakaDevOps <123265568+MakaDevOps@users.noreply.github.com>
2023-05-28 20:58:22 +0100

    Create maka.groovy

---
+++ vars/maka.groovy
@@ -0,0 +1,15 @@
+def call(String stageName){
+
+  if ("${stageName}" == "Build")
+  {
+    sh "mvn clean package"
+  }
+  else is ("${stageName}" == "SonaQube Report")
+  {
+    sh "mvn clean sonar:sonar"
+  }
+   else is ("${stageName}" == "Upload Into Nexus")
+  {
+    sh "mvn clean deploy"
+  }
+}
